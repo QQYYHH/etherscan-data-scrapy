ad5fa2c957e2c6ad0b580ba8801f2bc54bbcb098
milktime <1014708050@qq.com>
2021-08-16 22:16:34 +0800

    增加错误日志记录

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from scrapy import cmdline
 import sys
 
 if len(sys.argv) <= 1 or sys.argv[1] == 'block':
-    cmdline.execute('scrapy crawl etherscan -O block.json'.split())
+    cmdline.execute('scrapy crawl etherscan -s LOG_FILE=block-error.log -O block.json'.split())
 else:
-    cmdline.execute('scrapy crawl get_tx -O tx.json'.split())
+    cmdline.execute('scrapy crawl get_tx -s LOG_FILE=tx-error.log -O tx.csv'.split())
